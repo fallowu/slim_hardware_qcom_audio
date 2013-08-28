4dcc68d8b45630db424196c1e837db962bd3504e
fallow5 <fallowu@gmail.com>
2013-08-28 21:33:19 +0800

    first commit

---
+++ Android.mk
@@ -0,0 +1,14 @@
+ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf)
+
+MY_LOCAL_PATH := $(call my-dir)
+
+ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+include $(MY_LOCAL_PATH)/legacy/Android.mk
+else
+include $(MY_LOCAL_PATH)/hal/Android.mk
+include $(MY_LOCAL_PATH)/voice_processing/Android.mk
+endif
+
+endif
+endif
